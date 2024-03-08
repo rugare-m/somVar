247f8a00d5242f740f82333064699560c726d779
Rugare Maruzani <88198662+rugare-m@users.noreply.github.com>
2024-03-08 14:16:56 +0000

    Update predictor.py

--- fastq/predictor.py
+++ fastq/predictor.py
@@ -80,8 +80,8 @@ if __name__ == "__main__":
     filter_vcf(merged, output_vcf, variant_identifiers)
 
     # Remove duplicate variants 
-    subprocess.run(['bcftools', 'norm', '-d', 'all', '-o', output_vcf.replace('.vcf.gz', 'hc.vcf.gz'), output_vcf])
+    #subprocess.run(['bcftools', 'norm', '-d', 'all', '-o', output_vcf.replace('.dedup.snps.vcf.gz"', '.hc.vcf.gz'), output_vcf])
     # Delete the original vcf file
-    subprocess.run(['rm', output_vcf])
+    #subprocess.run(['rm', output_vcf])
     print("Process completed successfully.", flush=True)
 #end
